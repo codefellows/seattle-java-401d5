6c8b70bfb127dab84316e807f6f18b784ca0d415
Michelle Ferreirae <mnfmnfm@gmail.com>
2019-08-19 17:00:15 -0700

    day 11 demo

---
+++ class-11/introToSpringDemo/src/main/java/com/ferreirae/introToSpringDemo/IntroToSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ferreirae.introToSpringDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroToSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroToSpringDemoApplication.class, args);
+	}
+
+}
---
+++ class-11/introToSpringDemo/src/test/java/com/ferreirae/introToSpringDemo/IntroToSpringDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ferreirae.introToSpringDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IntroToSpringDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
