9321b14d13c8822516dbedccdc7b1c8c1afc5181
Michelle Ferreirae <mnfmnfm@gmail.com>
2019-08-22 16:08:22 -0700

    integration testing

--- class-13/introToSpringDemo/src/test/java/com/ferreirae/introToSpringDemo/IntroToSpringDemoApplicationTests.java
+++ class-13/introToSpringDemo/src/test/java/com/ferreirae/introToSpringDemo/IntroToSpringDemoApplicationTests.java
@@ -2,15 +2,37 @@ package com.ferreirae.introToSpringDemo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+// help from https://spring.io/guides/gs/testing-web/
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class IntroToSpringDemoApplicationTests {
+
+	@Autowired
+	MockMvc mockMvc;
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testRootRoute_containsHelloWorldH1() throws Exception {
+		this.mockMvc
+				.perform(MockMvcRequestBuilders.get("/"))
+				.andDo(MockMvcResultHandlers.print())
+				.andExpect(MockMvcResultMatchers.status().isNotFound())
+				.andExpect(MockMvcResultMatchers.content().string(
+						org.hamcrest.Matchers.containsString("<h1>Hello world!</h1>")));
 	}
 
 }
