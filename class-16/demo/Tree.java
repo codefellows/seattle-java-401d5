b39d8a81ad0e4de95a07c708103f259e01909e13
Michelle Ferreirae <mnfmnfm@gmail.com>
2019-08-26 15:08:23 -0700

    end of tree demo:

--- class-16/demo/Tree.java
+++ class-16/demo/Tree.java
@@ -17,16 +17,31 @@ public class Tree<E> {
     //   2   6
     //  /
     // 7
-    Node<Integer> node7 = new Node<>(7, null, null);
-    Node<Integer> node2 = new Node<>(2, node7, null);
+    // 7, 2, 3, 6
+    // Node<Integer> node7 = new Node<>(7, null, null);
+    // Node<Integer> node2 = new Node<>(2, node7, null);
     
-    Node<Integer> nodes = new Node<>(3,
-      new Node<>(2, 
-        new Node<>(7, null, null),
-        null),
-      new Node<>(6, null, null)
-    );
-    Tree<Integer> t = new Tree<>(nodes);
+    // // note: nodes does not use the above-created nodes
+    // // it's okay
+    // Node<Integer> nodes = new Node<>(3,
+    //   new Node<>(2, 
+    //     new Node<>(7, null, null),
+    //     null),
+    //   new Node<>(6, null, null)
+    // );
+    // Tree<Integer> t = new Tree<>(nodes);
+    // System.out.println(t.inorder());
+
+    BinarySearchTree t = new BinarySearchTree();
+    //   5
+    //  / \
+    // 3   7
+    // /
+    // 1
+    t.addRec(5);
+    t.addRec(3);
+    t.addRec(1);
+    t.addRec(7);
     System.out.println(t.inorder());
   }
 
@@ -49,7 +64,81 @@ public class Tree<E> {
       return ans;
     }
   }
+}
 
+class BinarySearchTree extends Tree<Integer> {
+    public BinarySearchTree() {
+        super();
+    }
+
+    public BinarySearchTree(int firstValue) {
+        super(new Node(firstValue, null, null));
+    }
+
+    public void add(int numberToAdd) {
+        if (this.root == null) {
+            this.root = new Node<>(numberToAdd, null, null);
+        } else {
+            Node<Integer> current = this.root;
+            while(current != null) {
+                if(numberToAdd < current.value) {
+                    // it belongs on the left
+                    if(current.left == null) {
+                        // put it in that spot!
+                        current.left = new Node<>(numberToAdd, null, null);
+                        return;
+                    } else {
+                        current = current.left;
+                    }
+                } else {
+                    // it belongs on the right
+                    if(current.right == null) {
+                        current.right = new Node<>(numberToAdd, null, null);
+                        return;
+                    } else {
+                        current = current.right;
+                    }
+                }
+            }
+        }
+    }
+
+    public void addRec(int numberToAdd) {
+        
+        this.root = addRecFruitful(this.root, numberToAdd);
+    }
+
+    public Node<Integer> addRecFruitful(Node<Integer> current, int numberToAdd) {
+        if (current == null) {
+            return new Node(numberToAdd, null, null);
+        } else {
+            if (numberToAdd < current.value) {
+                current.left = addRecFruitful(current.left, numberToAdd);
+            } else if (numberToAdd > current.value) {
+                current.right = addRecFruitful(current.right, numberToAdd);
+            } else {
+                // they were equal
+                // do nothing
+            }
+            return current;
+        }
+    }
+
+    private void addRec(Node<Integer> current, int numberToAdd) {
+        if (numberToAdd < current.value) {
+            if (current.left == null) {
+                current.left = new Node(numberToAdd, null, null);
+            } else {
+                addRec(current.left, numberToAdd);
+            }
+        } else {
+            if (current.right == null) {
+                current.right = new Node(numberToAdd, null, null);
+            } else {
+                addRec(current.right, numberToAdd);
+            }
+        }
+    }
 }
 
 class Node<T> {
