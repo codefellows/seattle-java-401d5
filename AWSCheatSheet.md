eff9c803ffc92e13f1f782112412c7fcb9aa0cfc
Michelle Ferreirae <mnfmnfm@gmail.com>
2019-08-29 17:02:51 -0700

    end of day 19

---
+++ AWSCheatSheet.md
@@ -0,0 +1,25 @@
+# Deployment with EC2 and RDS
+
+## Background
+Deploying a web app to EC2 traditionally requires writing a startup script in Bash to do things like copy your app's .jar file from S3, install dependencies, set environment variables, set up Apache with port forwarding from 80 to your app, and start up Apache. This is a lot of manual effort, and we can use Elastic Beanstalk for deployment to avoid this manual work, but we do NOT want to use Elastic Beanstalk for database; it limits our possibilities and can lead to bad situations. This doc walks through the process of setting up RDS manually, deploying to EC2 with Elastic Beanstalk, and connecting the two.
+
+## Steps
+
+
+2. Set up your Java app to use an environment variable for the database URL, username, and password. For Java apps, this will mean modifying the `spring.datasource.url, spring.datasource.username, spring.datasource.password` lines in your `application.properties` file. While in that file, also ensure the line `server.port=5000` is present, and also check that the `ddl-auto` line is set as necessary. As an example, your `application.properties` file might look like:
+    ```
+    spring.datasource.url=${DATABASE_URL}
+    spring.datasource.username=${DATABASE_USERNAME}
+    spring.datasource.password=${DATABASE_PASSWORD}
+    server.port=5000
+    spring.jpa.hibernate.ddl-auto=update
+    ```
+3. Run `./gradlew bootJar` to generate the .jar file for your app.
+4. In the AWS console, go to Elastic Beanstalk and create your application.
+5. Within that application, create your environment. Select Java as the engine, and start with starter code.
+6. Under "More configuration options", add a database. Pick postgres as your engine, and specify a good, secure username and password.
+7. Hit "create environment". (This takes about 15 minutes.)
+8. Go to the "Configuration" page. Under "Database", make note of the "database endpoint".
+8. Set environment variables for DATABASE_USERNAME and DATABASE_PASSWORD, with the good, secure username and password you specified earlier. Also create the DATABASE_URL environment variable, with `jbdc:postgresql://{database endpoint from the previous step goes here}/ebdb`
+9. Upload your .jar file.
+10. victory?
---
+++ class-19/Tree.java
@@ -0,0 +1,250 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Tree<E> {
+  Node<E> root;
+
+  public Tree() {
+    this.root = null;
+  }
+
+  public Tree(Node<E> root) {
+    this.root = root;
+  }
+
+  public void addAtNextOpenSpot(E value) {
+      Node<E> newNode = new Node<>(value, null, null);
+      if (this.root == null) {
+          this.root = newNode;
+          return;
+      }
+      LinkedList<Node<E>> nodesToCheck = new LinkedList<>();
+      nodesToCheck.addLast(this.root);
+      while(true) {
+          Node<E> current = nodesToCheck.removeFirst();
+          if(current.left == null) {
+              current.left = newNode;
+              return;
+          } else if (current.right == null) {
+              current.right = newNode;
+              return;
+          } else {
+              nodesToCheck.addLast(current.left);
+              nodesToCheck.addLast(current.right);
+          }
+      }
+  }
+
+  public static int findMax(Tree<Integer> tree) {
+      if (tree.root == null) {
+          throw new IllegalArgumentException("Cannot find max in an empty tree!");
+      } else {
+          return findMax(tree.root);
+      }
+  }
+
+  public static int findMax(Node<Integer> node) {
+      // where might the max be?
+      // might be node.value
+      int ans = node.value;
+      // might be in the left subtree
+      if (node.left != null) {
+          // go find the answer on the left
+          // save whichever of ans, max on left is bigger
+          ans = Math.max(ans, findMax(node.left));
+      }
+      if(node.right != null) {
+          ans = Math.max(ans, findMax(node.right));
+      }
+      // might be in the right subtree
+      // find the max from those three spots
+      // return it
+      return ans;
+  }
+
+  public static void main(String[] args) {
+    //     3
+    //    / \
+    //   2   6
+    //  /
+    // 7
+    // 7, 2, 3, 6
+    // Node<Integer> node7 = new Node<>(7, null, null);
+    // Node<Integer> node2 = new Node<>(2, node7, null);
+    
+    // // note: nodes does not use the above-created nodes
+    // // it's okay
+    // Node<Object> nodes = new Node<>(3,
+    //   new Node<>(2, 
+    //     new Node<>(7, null, null),
+    //     null),
+    //   new Node<>(6, null, null)
+    // );
+    Tree<Object> t = new Tree<>();
+    t.addAtNextOpenSpot(3);
+    t.addAtNextOpenSpot(2);
+    t.addAtNextOpenSpot(6);
+    t.addAtNextOpenSpot(10);
+    t.addAtNextOpenSpot(15);
+    System.out.println(fizzBuzzTree(t));
+
+    // BinarySearchTree t = new BinarySearchTree();
+    // //   5
+    // //  / \
+    // // 3   7
+    // // /
+    // // 1
+    // t.addRec(5);
+    // t.addRec(3);
+    // t.addRec(1);
+    // t.addRec(7);
+    // System.out.println(t.inorder());
+  }
+
+  public ArrayList<E> inorder() {
+    return inorder(this.root);
+  }
+
+  private ArrayList<E> inorder(Node<E> current) {
+    // base case: current node is null
+    if (current == null) {
+      return new ArrayList<E>();
+    } else {
+      ArrayList<E> leftAns = inorder(current.left);
+      ArrayList<E> rightAns = inorder(current.right);
+
+      ArrayList<E> ans = new ArrayList<>();
+      ans.addAll(leftAns);
+      ans.add(current.value);
+      ans.addAll(rightAns);
+      return ans;
+    }
+  }
+
+  public static Tree<Object> fizzBuzzTree(Tree<Object> treeToFizzBuzz) {
+      fizzBuzz(treeToFizzBuzz.root);
+      return treeToFizzBuzz;
+  }
+
+  private static void fizzBuzz(Node<Object> root) {
+      if (root != null) {
+          // do the normal fizzbuzz thing to the root
+          if ((int)root.value % 15 == 0) {
+              root.value = "Fizz Buzz";
+          } else if ((int)root.value % 5 == 0) {
+              root.value = "Buzz";
+          } else if ((int)root.value % 3 == 0) {
+              root.value = "Fizz";
+          }
+          // do the fizzbuzz to the left subtree
+          fizzBuzz(root.left);
+          // do the fizzbuzz to the right
+          fizzBuzz(root.right);
+      }
+  }
+
+  public String toString() {
+      return this.root.toString();
+  }
+}
+
+class BinarySearchTree extends Tree<Integer> {
+    public BinarySearchTree() {
+        super();
+    }
+
+    public BinarySearchTree(int firstValue) {
+        super(new Node(firstValue, null, null));
+    }
+
+    public void add(int numberToAdd) {
+        if (this.root == null) {
+            this.root = new Node<>(numberToAdd, null, null);
+        } else {
+            Node<Integer> current = this.root;
+            while(current != null) {
+                if(numberToAdd < current.value) {
+                    // it belongs on the left
+                    if(current.left == null) {
+                        // put it in that spot!
+                        current.left = new Node<>(numberToAdd, null, null);
+                        return;
+                    } else {
+                        current = current.left;
+                    }
+                } else {
+                    // it belongs on the right
+                    if(current.right == null) {
+                        current.right = new Node<>(numberToAdd, null, null);
+                        return;
+                    } else {
+                        current = current.right;
+                    }
+                }
+            }
+        }
+    }
+
+    public void addRec(int numberToAdd) {
+        
+        this.root = addRecFruitful(this.root, numberToAdd);
+    }
+
+    public Node<Integer> addRecFruitful(Node<Integer> current, int numberToAdd) {
+        if (current == null) {
+            return new Node(numberToAdd, null, null);
+        } else {
+            if (numberToAdd < current.value) {
+                current.left = addRecFruitful(current.left, numberToAdd);
+            } else if (numberToAdd > current.value) {
+                current.right = addRecFruitful(current.right, numberToAdd);
+            } else {
+                // they were equal
+                // do nothing
+            }
+            return current;
+        }
+    }
+
+    private void addRec(Node<Integer> current, int numberToAdd) {
+        if (numberToAdd < current.value) {
+            if (current.left == null) {
+                current.left = new Node(numberToAdd, null, null);
+            } else {
+                addRec(current.left, numberToAdd);
+            }
+        } else {
+            if (current.right == null) {
+                current.right = new Node(numberToAdd, null, null);
+            } else {
+                addRec(current.right, numberToAdd);
+            }
+        }
+    }
+}
+
+class Node<T> {
+  T value;
+  Node<T> left;
+  Node<T> right;
+
+  public Node() {
+    this.value = null;
+    this.left = null;
+    this.right = null;
+  }
+
+  public Node(T value, Node<T> left, Node<T> right) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
+
+  public String toString() {
+      return String.format("( %s %s %s )", 
+        this.left == null ? "." : this.left.toString(),
+        this.value,
+        this.right ==  null? "." : this.right.toString()
+      );
+  }
+}
